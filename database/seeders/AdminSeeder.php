3071309d544bb181743a27f5567dd459e96e603c
Maila Aziza <mailaazizaaiyub.com>
2025-11-23 14:09:23 +0700

    commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@siakad.com',
+            'password' => Hash::make('passwordadmin'),
+        ]);
+    }
+}
